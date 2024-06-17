6e2d93cc0d0ec19e048c10a6e39f7282ecd717b3
Guilherme Santos Silva <guilherme.ssilva256@gmail.com>
2024-06-17 11:18:34 -0300

    Add: Desafio POO Cami-la

---
+++ Desafio_Abstraindo_BootCamp/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,27 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo {
+    // atributos
+    private LocalDate data;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + data + "]";
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+}
