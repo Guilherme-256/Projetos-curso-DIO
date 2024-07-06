7008ed76a23fb8dabeb397d92a049693cebe4b53
Guilherme Santos Silva <guilherme.ssilva256@gmail.com>
2024-07-06 10:43:00 -0300

    WebApi

---
+++ my_first_web_api/src/main/java/dio/my_first_web_api/controller/UsuarioController.java
@@ -0,0 +1,23 @@
+package dio.my_first_web_api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dio.my_first_web_api.model.Usuario;
+import dio.my_first_web_api.repository.UsuarioRepository;
+
+@RestController
+public class UsuarioController {
+
+@Autowired
+private UsuarioRepository repository;
+
+@GetMapping("/users")
+public List<Usuario> getUser (){
+return repository.listAll();
+}
+
+}
